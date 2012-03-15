665c3b1719b90a010f85b7b4fba46771c01104b0
Flemming Frandsen <dren.dk@gmail.com>
2012-03-15 16:04:20 +0100

    Compiled for 32 bit OSX

--- src/dk/dren/hunspell/HunspellMain.java
+++ src/dk/dren/hunspell/HunspellMain.java
@@ -4,9 +4,6 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.ListIterator;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 /**
  * Simple testing and native build utility class, not useful in applications.
  *
@@ -18,24 +15,11 @@ import javax.swing.JLabel;
 
 public class HunspellMain {
 
-	static JFrame frame;
-	static JLabel label;
-	static String output;
 	private static void println(String msg) {
 		System.err.println(msg);
-		if (frame != null) {
-			output += msg + "<br>";
-			label.setText("<html>"+output+"</html>");
-			frame.pack();
-		}
 	}
 	private static void print(String msg) {
 		System.err.print(msg);
-		if (frame != null) {
-			output += msg;
-			label.setText("<html>"+output+"</html>");
-			frame.pack();
-		}
 	}
 
     public static void main(String[] args) {
@@ -44,18 +28,6 @@ public class HunspellMain {
 				System.out.println(Hunspell.libName());
 			
 			} else { 
-
-				try {
-					frame = new JFrame("Testing Hunspell");
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					label = new JLabel("Output from Hunspell...");
-					output = "";
-					frame.getContentPane().add(label);
-					frame.pack();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					frame = null;
-				}
 
 				System.err.println("Loading Hunspell");
 				String dir = "/home/ff/projects/hunspell-java";
